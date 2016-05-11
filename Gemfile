3552420feb523253b0f9429a1fd120b2ea9b86b5
rymcmahon <toedragdangle9@gmail.com>
2016-05-11 09:02:26 -0500

    added client new form, show page, bootstrap-sass, and font awesome

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.5'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
+# Sass-powered Bootstrap 3
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -22,8 +24,10 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-#webserver
+# webserver
 gem 'puma'
+# font-awesome-rails
+gem "font-awesome-rails"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,24 +1,50 @@
 class ClientsController < ApplicationController
 
   def index
+    @clients = Client.all
   end
 
   def show
+    @client = Client.find(params[:id])
   end
 
   def new
+    @client = Client.new
   end
 
   def edit
+    @client = Client.find(params[:id])
   end
 
   def create
+    @client = Client.new(client_params)
+    if @client.save
+      redirect_to @client, notice: "New Client Created!"
+    else
+      render 'new'
+    end
   end
 
   def update
+    @client = Client.find(params[:id])
+    if @client.update(client_params)
+      redirect_to @client
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+    redirect_to clients_path, alert: "Client Destroyed!"
   end
+
+  private
+
+    def client_params
+      params.require(:client).permit(:first_name, :last_name, :street_address, :unit,
+      :phone_one, :phone_two, :email_one, :email_two)
+    end
 
 end
