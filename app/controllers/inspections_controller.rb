2607e775fa30de8b08bc19e8e4ab85e298d7c45b
rymcmahon <toedragdangle9@gmail.com>
2016-05-15 15:24:54 -0500

    added form partial for client and inspections

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -16,19 +16,27 @@ class InspectionsController < ApplicationController
 
   def edit
     @inspection = Inspection.find(params[:id])
+    @client = Client.find(params[:client_id])
   end
 
   def create
     @client = Client.find(params[:client_id])
     @inspection = Inspection.new(inspection_params)
     if @inspection.save
-      redirect_to client_inspection_path(@client, @inspection), notice: "New Inscpection Saved!"
+      redirect_to client_inspection_path(@client, @inspection), notice: "New Inspection Saved!"
     else
       render new_client_inspection_path
     end
   end
 
   def update
+    @client = Client.find(params[:client_id])
+    @inspection = Inspection.find(params[:id])
+    if @inspection.update(inspection_params)
+      redirect_to client_inspection_path(@client, @inspection), notice: "Inspection Updated!"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -37,7 +45,12 @@ class InspectionsController < ApplicationController
   private
 
     def inspection_params
-      params.require(:inspection).permit(:mail, :front_door)
+      params.require(:inspection).permit(:mail, :front_door, :security_system, :water_main,
+      :water_heater, :garage_access_door, :garage_overhead_door, :outdoor_trash, :indoor_trash,
+      :breaker_box, :washer, :dryer, :smoke_detectors, :ceiling_fans, :humidistat,
+      :thermostat, :cabinets_drawers, :refrigerator, :freezer, :garbage_disposal, :dishwasher,
+      :faucets, :toilets, :doors_windows, :ceilings_walls, :screen_doors, :pool, :exterior,
+      :pests, :general_cleanliness, :carpet_cleanliness, :window_cleanliness, :patio_cleanliness, :client_id)
     end
 
 end
