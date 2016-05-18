44abe08b6e99b034cb57a03f79ba84d58e0464cf
rymcmahon <toedragdangle9@gmail.com>
2016-05-17 19:52:06 -0500

    added inspection email

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -24,6 +24,7 @@ class InspectionsController < ApplicationController
     @client = Client.find(params[:client_id])
     @inspection = Inspection.new(inspection_params)
     if @inspection.save
+      ClientMailer.inspection_email(@client, @inspection).deliver_now
       redirect_to client_inspection_path(@client, @inspection), notice: "New Inspection Saved!"
     else
       render new_client_inspection_path
--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -5,4 +5,10 @@ class ClientMailer < ApplicationMailer
     @client = client
     mail(to: @client.email_one, subject: "Welcome to John's Home Watch", reply_to: 'magsups2002@yahoo.com')
   end
+
+  def inspection_email(client, inspection)
+    @client = client
+    @inspection = inspection
+    mail(to: @client.email_one, subject: "Home Inspection Report", reply_to: 'magsups2002@yahoo.com')
+  end
 end
