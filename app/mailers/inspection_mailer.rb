d488048cc41e5d1c8698a849f915bee08b7d8d20
rymcmahon <toedragdangle9@gmail.com>
2016-05-17 19:28:29 -0500

    generated inspection mailer

---
+++ app/mailers/inspection_mailer.rb
@@ -0,0 +1,2 @@
+class InspectionMailer < ApplicationMailer
+end
---
+++ test/mailers/inspection_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InspectionMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
