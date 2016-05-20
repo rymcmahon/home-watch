35fdcee091d580e46dbd62afa5fc697a2568d142
rymcmahon <toedragdangle9@gmail.com>
2016-05-20 08:52:35 -0500

    generated devise views and added invite to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+    :trackable, :validatable
+
+  validate :validate_invite, :on => :create
+  attr_accessor :invite
+
+  def validate_invite
+   if self.invite != ENV["invitation_code"]
+     self.errors[:base] << "The Invitation Code is Incorrect"
+   end
+  end
 end
