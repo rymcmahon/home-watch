8393ab118432bc7826a72d63d88591e8ae350075
rymcmahon <toedragdangle9@gmail.com>
2016-05-26 17:19:03 -0500

    added validations to inspection form

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,4 +1,10 @@
 class Inspection < ActiveRecord::Base
   belongs_to :client
-  validates :mail, presence: true
+  validates :mail, :front_door, :security_system, :water_main, :water_heater,
+  :garage_access_door, :garage_overhead_door, :outdoor_trash, :indoor_trash,
+  :breaker_box, :smoke_detectors, :ceiling_fans, :humidistat,
+  :thermostat, :cabinets_drawers, :refrigerator, :freezer, :garbage_disposal,
+  :dishwasher, :faucets, :toilets, :doors_windows, :ceilings_walls, :screen_doors,
+  :pool, :exterior, :pests, :general_cleanliness, :carpet_cleanliness, :window_cleanliness,
+  :patio_cleanliness, presence: true
 end
