100912eeca4b983e9c71d9ae3ef325acfc8509ff
rymcmahon <toedragdangle9@gmail.com>
2016-05-26 08:29:42 -0500

    added dependent destroy on clients and updated delete confirm text

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
-  has_many :inspections
+  has_many :inspections, :dependent => :destroy
   validates :email_one, uniqueness: true
   validates :first_name, :last_name, :street_address, :email_one, presence: true
 end
